0f077cb69c98d1cf86c0f4e05aaddebd312d9f6c
Jing Ai <jinga@google.com>
2018-06-27 12:24:40 -0700

    Move PTP CNI spec generation logic out of netd.

--- cmd/netd/netd.go
+++ cmd/netd/netd.go
@@ -17,86 +17,11 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
 	"time"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-
-	"github.com/golang/glog"
 )
 
 func main() {
-	nodeName, err := os.Hostname()
-	if err != nil {
-		glog.Error("failed to get node name: %v", err)
-		return
-	}
-
-	cniPath, cniConfigTemplate := os.Getenv("NETD_CNI_CONFIG_PATH"), os.Getenv("NETD_CNI_NETWORK_CONFIG_TEMPLATE")
-	if len(cniPath) == 0 || len(cniConfigTemplate) == 0 {
-		glog.Warningf("failed to read either env NETD_CNI_CONFIG_PATH: %q or env NETD_CNI_NETWORK_CONFIG_TEMPLATE: %q", cniPath, cniConfigTemplate)
-		return
-	}
-
-	client, err := k8sClient()
-	if err != nil {
-		return
-	}
-
-	var cidr string
 	for {
-		_, err := os.Stat(cniPath)
-		if err == nil || !os.IsNotExist(err) {
-			time.Sleep(10 * time.Second)
-			continue
-		}
-
-		if cidr == "" {
-			cidr, err = podCIDR(client, nodeName)
-			if err != nil {
-				continue
-			}
-		}
-
-		glog.Infof("Installing CNI on %q", nodeName)
-
-		cniConfig := fmt.Sprintf(cniConfigTemplate, cidr)
-		if err := ioutil.WriteFile(cniPath, []byte(cniConfig), 0644); err != nil {
-			glog.Errorf("failed to write CNI config %q to %q: %v", cniConfig, cniPath, err)
-			continue
-		}
+		time.Sleep(10 * time.Second)
 	}
-}
-
-func k8sClient() (*kubernetes.Clientset, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		glog.Errorf("failed to create in-cluster config: %v", err)
-		return nil, err
-	}
-
-	client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		glog.Errorf("failed to create the K8s client: %v", err)
-		return nil, err
-	}
-	return client, nil
-}
-
-func podCIDR(client *kubernetes.Clientset, nodeName string) (string, error) {
-	node, err := client.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
-	if err != nil {
-		glog.Errorf("failed to get node %q's spec: %v", nodeName, err)
-		return "", err
-	}
-
-	if node.Spec.PodCIDR == "" {
-		err = fmt.Errorf("not found podCIDR for node %q", nodeName)
-		return "", err
-	}
-
-	return node.Spec.PodCIDR, nil
 }
